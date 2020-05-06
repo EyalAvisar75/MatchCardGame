cf877aa0ed1b7d3c075b7dc76b08728bb623e0b5
Eyal Avisar <enfer0666@yahoo.com>
2020-05-06 11:29:27 +0300

    Initial Commit

---
+++ CardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardGame
+//
+//  Created by eyal avisar on 06/05/2020.
+//  Copyright © 2020 eyal avisar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
